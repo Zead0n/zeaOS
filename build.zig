6130ae4013127e75fe4961ac3b61cafbd809f01f
Zead0n <141008297+Zead0n@users.noreply.github.com>
2025-04-01 01:58:48 -0500

    Remove build folder

--- build.zig
+++ build.zig
@@ -3,10 +3,10 @@ const std = @import("std");
 const options = @import("build/options.zig");
 
 pub fn build(b: *std.Build) void {
-    const zeaTarget = options.zeaTargetOption(b);
+    const zeaTarget = b.option(ZeaTarget, "arch", "Cpu architecture (Defaults to x86)") orelse ZeaTarget.x86;
 
-    const target = zeaTarget.getResolvedTarget(b);
-    const optimize = options.optimizeOption(b);
+    const target = b.resolveTargetQuery(zeaTarget.getTargetQuery());
+    const optimize = b.standardOptimizeOption(.{});
 
     const exeOptions = std.Build.ExecutableOptions{
         .name = "kernel.elf",
@@ -24,3 +24,36 @@ pub fn build(b: *std.Build) void {
     const kernel_step = b.step("kernel", "Build the kernel");
     kernel_step.dependOn(&kernel.step);
 }
+
+const ZeaTarget = enum {
+    x86,
+
+    pub fn getTargetQuery(self: ZeaTarget) std.Target.Query {
+        const x86 = blk: {
+            var disabled_features = std.Target.Cpu.Feature.Set.empty;
+            var enabled_features = std.Target.Cpu.Feature.Set.empty;
+
+            disabled_features.addFeature(@intFromEnum(std.Target.x86.Feature.mmx));
+            disabled_features.addFeature(@intFromEnum(std.Target.x86.Feature.sse));
+            disabled_features.addFeature(@intFromEnum(std.Target.x86.Feature.sse2));
+            disabled_features.addFeature(@intFromEnum(std.Target.x86.Feature.avx));
+            disabled_features.addFeature(@intFromEnum(std.Target.x86.Feature.avx2));
+
+            enabled_features.addFeature(@intFromEnum(std.Target.x86.Feature.soft_float));
+
+            break :blk std.Target.Query{
+                .cpu_arch = .x86,
+                .os_tag = .freestanding,
+                .abi = .none,
+                .cpu_features_sub = disabled_features,
+                .cpu_features_add = enabled_features,
+            };
+        };
+
+        const targetQuery = switch (self) {
+            .x86 => x86,
+        };
+
+        return targetQuery;
+    }
+};
