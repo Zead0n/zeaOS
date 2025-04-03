a40446a3a7287b2473f6fbb10ee1ab3433880042
Zead0n <141008297+Zead0n@users.noreply.github.com>
2025-04-03 01:17:48 -0500

    Revert to basic kernel

--- kernel/kernel.zig
+++ kernel/kernel.zig
@@ -1,5 +1,38 @@
 const console = @import("io/console.zig");
 
+const MultibootHeader = packed struct {
+    magic: u32,
+    flags: u32,
+    checksum: u32,
+    padding: u32 = 0,
+};
+
+export const multiboot_header align(4) linksection(".multiboot") = multiboot: {
+    const ALIGN = 1 << 0;
+    const MEMINFO = 1 << 1;
+    const MAGIC = 0x1BADB002;
+    const FLAGS = ALIGN | MEMINFO;
+
+    break :multiboot MultibootHeader{
+        .magic = MAGIC,
+        .flags = FLAGS,
+        .checksum = -(MAGIC + FLAGS),
+    };
+};
+
+var stack_bytes: [16 * 1024]u8 align(16) linksection(".bss") = undefined;
+
+export fn _start() callconv(.Naked) noreturn {
+    asm volatile (
+        \\ movl %[stack_top], %%esp
+        \\ movl %%esp, %%ebp
+        \\ call %[kmain:P]
+        :
+        : [stack_top] "i" (@as([*]align(16) u8, @ptrCast(&stack_bytes)) + @sizeOf(@TypeOf(stack_bytes))),
+          [kmain] "X" (&kmain),
+    );
+}
+
 export fn kmain() callconv(.C) noreturn {
     console.initialize();
     console.puts("Hello ZeaOS!");
--- kernel/platform/arch.zig
+++
@@ -1,8 +0,0 @@
-const builtin = @import("builtin");
-
-const platform = switch (builtin.cpu.arch) {
-    .x86 => @import("x86/x86.zig"),
-    else => unreachable,
-};
-
-pub usingnamespace platform;
