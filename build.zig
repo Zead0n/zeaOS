2baf39f13b3607c28a9a78024ad94b8bdbe1ae1d
Zead0n <141008297+Zead0n@users.noreply.github.com>
2025-04-05 19:38:49 -0500

    Improve build.zig

--- build.zig
+++ build.zig
@@ -1,35 +1,36 @@
 const std = @import("std");
+const zon = @import("builtin");
 
 pub fn build(b: *std.Build) void {
     const zea = b.option(Zea, "arch", "Cpu architecture (Defaults to x86)") orelse Zea.x86;
 
+    const kernel_step = b.step("kernel", "Build the kernel");
     const kernel = zea.buildKernel(b);
     b.installArtifact(kernel);
-    const kernel_step = b.step("kernel", "Build the kernel");
     kernel_step.dependOn(&kernel.step);
 
-    const grub_iso = buildIso(b, kernel);
     const grub_step = b.step("grub", "Build grub iso");
+    const grub_iso = buildIso(b, kernel);
     grub_step.dependOn(&grub_iso.step);
 
-    const qemu_cmd = b.addSystemCommand(&.{ "qemu-system-x86_64", "-kernel" });
+    const qemu_step = b.step("qemu", "Build and run the kernel in qemu");
+    const qemu_cmd = b.addSystemCommand(&.{ "qemu-system-i386", "-kernel" });
     qemu_cmd.addArtifactArg(kernel);
     qemu_cmd.step.dependOn(&kernel.step);
-    const qemu_step = b.step("qemu", "Build and run the kernel in qemu");
     qemu_step.dependOn(&qemu_cmd.step);
 }
 
-pub fn buildIso(b: *std.Build, kernel: *std.Build.Step.Compile) *std.Build.Step.InstallFile {
+fn buildIso(b: *std.Build, kernel: *std.Build.Step.Compile) *std.Build.Step.InstallFile {
     const grub_kernel = b.pathJoin(&.{ "boot", kernel.out_filename });
-    const gen_grub =
-        \\ menuentry "ZeaOS" {{
-        \\     multiboot /{s}
-        \\ }}
-    ;
+    const gen_grub = b.fmt(
+        \\menuentry "ZeaOS" {{
+        \\    multiboot /{s}
+        \\}}
+    , .{grub_kernel});
 
     const boot_dir = b.addWriteFiles();
     _ = boot_dir.addCopyFile(kernel.getEmittedBin(), grub_kernel);
-    _ = boot_dir.add(b.pathJoin(&.{ "boot", "grub", "grub.cfg" }), b.fmt(gen_grub, .{grub_kernel}));
+    _ = boot_dir.add(b.pathJoin(&.{ "boot", "grub", "grub.cfg" }), gen_grub);
 
     const grub_iso_cmd = b.addSystemCommand(&.{"grub-mkrescue"});
     const iso_file = grub_iso_cmd.addPrefixedOutputFileArg("--output=", "zeaos.iso");
