94c0bf58d7e899da54592275f1b71af86f892790
Zead0n <141008297+Zead0n@users.noreply.github.com>
2025-04-21 17:36:44 -0500

    Add qemu-i386 step

--- build.zig
+++ build.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const arch = b.option(Arch, "arch", "Cpu architecture (Defaults to x86_64)") orelse Arch.x86_64;
+    const arch = b.option(Arch, "arch", "Cpu architecture (Defaults to x86)") orelse Arch.x86;
     const optimize = b.standardOptimizeOption(.{});
 
     const kernel_module = b.createModule(.{
@@ -11,28 +11,35 @@ pub fn build(b: *std.Build) void {
     });
 
     switch (arch) {
-        .x86_64 => {
+        .x86 => {
             kernel_module.red_zone = false;
             kernel_module.code_model = .kernel;
         },
     }
 
     const kernel = b.addExecutable(.{
-        .name = "kernel",
+        .name = "kernel.elf",
         .root_module = kernel_module,
     });
+    kernel.setLinkerScript(b.path("kernel/linker.ld"));
 
     const kernel_step = b.step("kernel", "Build the kernel");
     b.installArtifact(kernel);
-    kernel_step.dependOn(&kernel.step);
+    kernel_step.dependOn(b.getInstallStep());
+
+    const qemu_i386_cmd = b.addSystemCommand(&.{ "qemu-system-i386", "-kernel" });
+    qemu_i386_cmd.addArtifactArg(kernel);
+    qemu_i386_cmd.step.dependOn(&kernel.step);
+    const qemu_i386_step = b.step("qemu-i386", "Build and run kernel on i386");
+    qemu_i386_step.dependOn(&qemu_i386_cmd.step);
 }
 
 const Arch = enum {
-    x86_64,
+    x86,
 
     pub fn toStdArch(self: @This()) std.Target.Cpu.Arch {
         return switch (self) {
-            .x86_64 => .x86_64,
+            .x86 => .x86,
         };
     }
 
@@ -44,7 +51,7 @@ const Arch = enum {
         };
 
         switch (self) {
-            .x86_64 => {
+            .x86 => {
                 const Target = std.Target.x86;
 
                 query.cpu_features_add = Target.featureSet(&.{ .popcnt, .soft_float });
