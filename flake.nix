7f821f0f051aa4a8bc61c0ce77df2f6b79b1131d
Zead0n <141008297+Zead0n@users.noreply.github.com>
2025-07-12 04:46:15 -0500

    Format flake.nix

--- flake.nix
+++ flake.nix
@@ -1,19 +1,13 @@
 {
   description = "Nix flake for developing zeaOS";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
 
   outputs = { self, nixpkgs }:
-  let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in
-  with pkgs;
-  {
-    devShells.${system}.default = mkShell {
-      packages = [ zig qemu ];
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in with pkgs; {
+      devShells.${system}.default = mkShell { packages = [ zig qemu ]; };
     };
-  };
 }
