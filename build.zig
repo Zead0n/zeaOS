5f66e3899891f1cc517c08e33a1bcfab8022e9b6
Zead0n <141008297+Zead0n@users.noreply.github.com>
2025-09-27 23:36:32 -0500

    Give up making custom bootloader

--- build.zig
+++ build.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
-const arch_util = @import("build/arch.zig");
-const builder_util = @import("build/builder.zig");
+const arch_util = @import("utils/arch.zig");
+const builder_util = @import("utils/builder.zig");
 
 const Arch = arch_util.Arch;
 const Builder = builder_util.Builder;
@@ -14,21 +14,15 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     };
 
-    const bootloader = builder.buildBootloader(b);
     const kernel = builder.buildKernel(b);
 
-    const bootloader_install = b.addInstallBinFile(bootloader.getEmittedBin(), bootloader.name);
     const kernel_install = b.addInstallArtifact(kernel, .{});
-
-    const install_step = b.getInstallStep();
-    install_step.dependOn(&bootloader_install.step);
-    install_step.dependOn(&kernel_install.step);
-
-    // Bootloader step
-    const bootloader_step = b.step("bootloader", "Build the bootloader");
-    bootloader_step.dependOn(&bootloader_install.step);
 
     // Kernel step
     const kernel_step = b.step("kernel", "Build the kernel");
     kernel_step.dependOn(&kernel_install.step);
+
+    // Install step
+    const install_step = b.getInstallStep();
+    install_step.dependOn(&kernel_install.step);
 }
--- utils/builder.zig
+++ utils/builder.zig
@@ -4,27 +4,6 @@ pub const Builder = struct {
     target: std.Build.ResolvedTarget,
     optimize: std.builtin.OptimizeMode,
 
-    pub fn buildBootloader(self: Builder, b: *std.Build) *std.Build.Step.Compile {
-        const boot_dir = b.path("boot/x86");
-
-        const first_stage_dir = boot_dir.path(b, "stage1");
-
-        const boot_mod = b.createModule(.{
-            .target = self.target,
-            .optimize = self.optimize,
-            .red_zone = false,
-        });
-        boot_mod.addAssemblyFile(first_stage_dir.path(b, "boot.s"));
-
-        const boot_bin = b.addExecutable(.{
-            .name = "bootloader.bin",
-            .root_module = boot_mod,
-        });
-        boot_bin.setLinkerScript(first_stage_dir.path(b, "link.ld"));
-
-        return boot_bin;
-    }
-
     pub fn buildKernel(self: Builder, b: *std.Build) *std.Build.Step.Compile {
         const kernel_dir = b.path("kernel");
 
