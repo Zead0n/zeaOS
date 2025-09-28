638150c6ad9b4699a7b614f2f9efe59c55af6c65
Zead0n <141008297+Zead0n@users.noreply.github.com>
2025-09-27 23:27:28 -0500

    Update kmain callconv

--- kernel/kernel.zig
+++ kernel/kernel.zig
@@ -30,7 +30,7 @@ export fn _start() callconv(.naked) noreturn {
     );
 }
 
-fn kmain() callconv(.C) noreturn {
+fn kmain() callconv(.c) noreturn {
     console.initialize();
     console.puts("Hello ZeaOS!");
     while (true) {}
