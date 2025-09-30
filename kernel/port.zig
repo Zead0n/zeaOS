58840458affa84eb668453382aa76fe79a0a5e6c
Zead0n <141008297+Zead0n@users.noreply.github.com>
2025-09-30 04:23:29 -0500

    Create ports.zig

---
+++ kernel/port.zig
@@ -0,0 +1,14 @@
+pub fn outb(port: u16, val: u8) void {
+    asm volatile ("outb %[val], %[port]"
+        :
+        : [port] "{al}" (port),
+          [val] "{dx}" (val),
+    );
+}
+
+pub fn inb(port: u16) u8 {
+    asm volatile ("inb %[port], %[result]"
+        : [result] "={al}" (-> u8),
+        : [port] "{al}" (port),
+    );
+}
