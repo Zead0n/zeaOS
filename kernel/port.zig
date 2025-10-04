c20acbfc9e9eeef83cd0df57de6c7495be27811a
Zead0n <141008297+Zead0n@users.noreply.github.com>
2025-10-04 01:00:15 -0500

    Implement inw and outw for port.zig

--- kernel/port.zig
+++ kernel/port.zig
@@ -12,3 +12,18 @@ pub fn inb(port: u16) u8 {
         : [port] "{al}" (port),
     );
 }
+
+pub fn outw(port: u16, val: u8) void {
+    asm volatile ("outb %[val], %[port]"
+        :
+        : [port] "{ax}" (port),
+          [val] "{dx}" (val),
+    );
+}
+
+pub fn inw(port: u16) u8 {
+    asm volatile ("inb %[port], %[result]"
+        : [result] "={ax}" (-> u8),
+        : [port] "{al}" (port),
+    );
+}
