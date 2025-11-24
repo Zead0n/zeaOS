ef15a775eee46530d14aa13c1267c41cb2a359de
Zead0n <141008297+Zead0n@users.noreply.github.com>
2025-11-24 02:14:02 -0600

    Update arch compile error message

--- kernel/arch.zig
+++ kernel/arch.zig
@@ -2,5 +2,5 @@ const builtin = @import("builtin");
 
 pub const platform = switch (builtin.cpu.arch) {
     .x86 => @import("./arch/x86/platform.zig"),
-    else => @compileError("No lib for Architecture"),
+    else => @compileError("No platform for specified Architecture"),
 };
